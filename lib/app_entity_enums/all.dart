4ad3e9c0522df429ea55d6e726751a0241505060
Dewey Gaedcke <dewey@pathoz.com>
2022-02-28 12:33:53 -0600

    updated imports

--- lib/app_entity_enums/all.dart
+++ lib/app_entity_enums/all.dart
@@ -6,7 +6,6 @@ import '../enums/all.dart';
 import '../util/type_cast.dart';
 
 part 'ui_slot_in_wid_area.dart';
-// part 'db_row_type.dart';
 part 'ui_screen.dart';
 part 'tbl_fld_name.dart';
 part 'ui_screen_area.dart';
