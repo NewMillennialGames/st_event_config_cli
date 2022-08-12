ed22ae06708d1c4baafdc6aab00b54d1a1d880b4
Dewey Gaedcke <dewey@pathoz.com>
2022-08-12 13:27:04 -0700

    added 2 tests

--- st_ui_builder/test/load_test.dart
+++ st_ui_builder/test/load_test.dart
@@ -13,9 +13,9 @@ void main() {
   late StUiBuilderFactory builderFactory;
 
   group('various methods on the ui factory', () {
-    test('validate loading json', () async {
+    test('validate loading json succeds to build factory', () async {
       //
-      builderFactory = await loadFactory('assets/one.json');
+      builderFactory = await loadFactory('assets/test.json');
       expect(builderFactory.marketViewIsGameCentricAndTwoPerRow, true);
       expect(builderFactory.marketViewRowsAreSingleAssetOnly, false);
     });
@@ -33,7 +33,7 @@ void main() {
         reason: 'json specifies driverVsField',
       );
       expect(
-        trdm.filterRules?.disableFiltering,
+        trdm.filterRules?.disableFiltering ?? true,
         false,
         reason: 'json HAS filtering rules',
       );
@@ -46,6 +46,11 @@ void main() {
         trdm.groupingRules?.item1.colName,
         DbTableFieldName.assetOrgName,
         reason: 'json specifies grouping on 2 fields; first is assetOrgName',
+      );
+      expect(
+        trdm.groupingRules?.item2?.colName,
+        DbTableFieldName.conference,
+        reason: 'json specifies grouping on 2 fields; 2nd is conference',
       );
     });
     test('', () async {
