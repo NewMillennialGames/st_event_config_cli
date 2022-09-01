0ee7413cb460f44f3c7ba3124420729db645d735
AchizuePriscilla <priscillaachizue@gmail.com>
2022-09-01 08:44:28 +0100

    Chore: Clean up row style widgets file, add unique class for AssetVsAssetRankedRowMktRsrch

--- st_ui_builder/lib/ui_builder/tv_config.dart
+++ st_ui_builder/lib/ui_builder/tv_config.dart
@@ -92,7 +92,7 @@ class TableviewConfigPayload {
           case TvAreaRowStyle.assetVsAsset:
             return AssetVsAssetRowMktResearchView.new;
           case TvAreaRowStyle.assetVsAssetRanked:
-            return AssetVsAssetRowMktResearchView.new;
+            return AssetVsAssetRankedRowMktResearchView.new;
           case TvAreaRowStyle.teamVsField:
             return TeamVsFieldRowMktResearchView.new;
           case TvAreaRowStyle.playerVsField:
