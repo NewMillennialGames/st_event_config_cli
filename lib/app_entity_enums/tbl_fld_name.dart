a903258dfbc51502ce758113b54ad390bdd73733
Crazelu <eberelucky64@gmail.com>
2022-09-15 21:42:35 +0100

    fix: Promotes filter bar title extraction to UI Builder, updates filtering and sorting logic, removes previous team hack

--- lib/app_entity_enums/tbl_fld_name.dart
+++ lib/app_entity_enums/tbl_fld_name.dart
@@ -4,8 +4,6 @@ part of AppEntities;
 enum DbTableFieldName {
   assetName,
   assetShortName,
-  teamName,
-  teamShortName,
   assetOrgName,
   conference,
   region,
@@ -19,44 +17,4 @@ enum DbTableFieldName {
   assetCurrentPrice,
   assetRankOrScore,
   assetPosition,
-}
-
-extension DbTableFieldNameExt1 on DbTableFieldName {
-  //
-  String get labelName {
-    switch (this) {
-      case DbTableFieldName.assetName:
-        return 'Player';
-      case DbTableFieldName.assetShortName:
-        return 'Player';
-      case DbTableFieldName.teamName:
-        return 'Team';
-      case DbTableFieldName.teamShortName:
-        return 'Team';
-      case DbTableFieldName.assetOrgName:
-        return 'Org';
-      case DbTableFieldName.conference:
-        return 'Conference';
-      case DbTableFieldName.region:
-        return 'All Regions';
-      // case DbTableFieldName.eventName:
-      //   return 'Event Name';
-      case DbTableFieldName.gameDate:
-        return 'All Dates';
-      case DbTableFieldName.gameTime:
-        return 'Game Time';
-      case DbTableFieldName.gameLocation:
-        return 'Location';
-      case DbTableFieldName.imageUrl:
-        return 'Avatar (select this to hide filter bar)';
-      case DbTableFieldName.assetOpenPrice:
-        return 'Open Price';
-      case DbTableFieldName.assetCurrentPrice:
-        return 'Current Price';
-      case DbTableFieldName.assetRankOrScore:
-        return 'Rank';
-      case DbTableFieldName.assetPosition:
-        return 'Position';
-    }
-  }
 }
